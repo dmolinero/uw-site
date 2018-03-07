894e6b6ae0e04e11202dcacf32de838f8569e8c9
dm1600 <molinero.david@gmail.com>
2018-03-06 22:33:49 -0800

    Update WTA page

---
+++ scripts/global.js
@@ -0,0 +1,12 @@
+
+// Open the menu
+function openMenu() {
+    var menu = document.getElementById('nav');
+    menu.classList.add('open');
+}
+
+// Close the menu
+function closeMenu() {
+    var menu = document.getElementById('nav');
+    menu.classList.remove('open');
+}
